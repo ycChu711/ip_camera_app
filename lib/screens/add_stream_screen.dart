c5e54ef32f94af244607620d3c2c7d158ab1f084
ycc711 <chu.yi-c@northeastern.edu>
2024-06-20 09:42:36 +0800

    Change add stream dialog RTSP url placeholder to Stream url.

--- lib/screens/add_stream_screen.dart
+++ lib/screens/add_stream_screen.dart
@@ -39,7 +39,7 @@ class _AddStreamScreenState extends State<AddStreamScreen> {
             TextField(
               controller: _urlController,
               decoration: const InputDecoration(
-                labelText: rtspUrlLabel,
+                labelText: streamUrlLabel,
               ),
             ),
           ],
