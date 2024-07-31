076e0bee71ad8f21799dd8f509614a38a5a3cea2
ycc711 <chu.yi-c@northeastern.edu>
2024-07-31 14:08:57 +0800

    and comments

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ import 'services/mqtt_service.dart';
 final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
     FlutterLocalNotificationsPlugin();
 
+// firebase messaging background handler
 Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
   await Firebase.initializeApp();
   showNotification(message.notification?.title, message.notification?.body);
@@ -22,6 +23,7 @@ Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
 void showNotification(String? title, String? body) async {
   const AndroidNotificationDetails androidPlatformChannelSpecifics =
       AndroidNotificationDetails(
+    // todo: change channel id, name, description
     'your_channel_id',
     'your_channel_name',
     channelDescription: 'your_channel_description',
@@ -42,13 +44,14 @@ void showNotification(String? title, String? body) async {
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  MediaKit.ensureInitialized();
+  await Firebase.initializeApp(); // Initialize Firebase
+  MediaKit.ensureInitialized(); // Initialize MediaKit
 
-  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+  FirebaseMessaging.onBackgroundMessage(
+      _firebaseMessagingBackgroundHandler); // Initialize Firebase Messaging
 
   const AndroidInitializationSettings initializationSettingsAndroid =
-      AndroidInitializationSettings('@mipmap/ic_launcher');
+      AndroidInitializationSettings('@mipmap/ic_launcher'); // todo: change icon
   const DarwinInitializationSettings initializationSettingsIOS =
       DarwinInitializationSettings(
     requestAlertPermission: true,
@@ -59,14 +62,17 @@ void main() async {
     android: initializationSettingsAndroid,
     iOS: initializationSettingsIOS,
   );
-  await flutterLocalNotificationsPlugin.initialize(initializationSettings);
+  await flutterLocalNotificationsPlugin.initialize(
+      initializationSettings); // Initialize Flutter Local Notifications
 
   final mqttService = MqttService(mqttServer,
-      mqttClientId); // Testing MQTT server IP address: '192.168.1.15', 'flutter_client'
+      mqttClientId); // Initialize MQTT Service with server and client id
   try {
     await mqttService.connect();
-    mqttService.subscribe(mqttTopic); //topic for testing: test/topic
-    runApp(MyApp(mqttService: mqttService));
+    mqttService.subscribe(mqttTopic); // Subscribe to MQTT topic
+    runApp(MyApp(
+        mqttService:
+            mqttService)); // Pass the MQTT service if the connection is successful
   } catch (e) {
     if (kDebugMode) {
       print('Failed to connect to MQTT server: $e');
@@ -83,7 +89,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Security Camera',
+      title: 'Security Camera', // App title
       theme: ThemeData(
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
